e261d490fe7a738b2a81a546cd94b20fa98cc4e3
Nelson <alexandrsushkov90@gmail.com>
2017-10-05 14:56:54 +0300

    barriers and groups test

--- app/src/main/java/dev/nelson/template/MainActivity.java
+++ app/src/main/java/dev/nelson/template/MainActivity.java
@@ -1,7 +1,13 @@
 package dev.nelson.template;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import dev.nelson.template.bariers.BarriersTestActivity;
+import dev.nelson.template.groups.GroupsTestActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_groups)
+    void onClickGroups(){
+        startActivity(new Intent(this, GroupsTestActivity.class));
+    }
+
+    @OnClick(R.id.btn_barriers)
+    void onClickBarriers(){
+        startActivity(new Intent(this, BarriersTestActivity.class));
     }
 }
---
+++ app/src/main/java/dev/nelson/template/bariers/BarriersTestActivity.java
@@ -0,0 +1,16 @@
+package dev.nelson.template.bariers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import dev.nelson.template.R;
+
+
+public class BarriersTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barriers_test);
+    }
+}
